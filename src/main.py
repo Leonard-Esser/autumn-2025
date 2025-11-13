114ae36d2a1f3de965e19d35e61eb10dccb17388
Leonard Esser <leonard.esser@posteo.net>
2025-11-13 09:07:18 +0100

    Add pseudocode and code for sampling module
    - Add function that mocks sampling one repo

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 import config
+from sampling import get_sample
 
 
 def main():
     print("Hello from autumn-2025!")
     print(f"The configured sample size is: {config.SAMPLE_SIZE}")
+    sample = get_sample()
+    print(sample)
 
 
 if __name__ == "__main__":
---
+++ src/sampling.py
@@ -0,0 +1,14 @@
+def get_sample():
+    return {get_full_name_of_some_repo()}
+
+
+def get_full_name_of_some_repo() -> str:
+    return "microsoft/vscode"
+
+
+def main():
+    print(get_sample())
+
+
+if __name__ == "__main__":
+    main()
