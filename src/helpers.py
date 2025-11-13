ceec83e07527a411db55a9984ab241f0a73a88a5
Leonard Esser <leonard.esser@posteo.net>
2025-11-13 15:40:01 +0100

    Add helper functions

--- src/helpers.py
+++ src/helpers.py
@@ -1,12 +1,44 @@
+from shutil import rmtree
+import os
 import subprocess
 
 from pathlib import Path
 
 
-def run_git_gc(working_dir: str):
-    if not path_is_git_dir(working_dir):
-        raise ValueError(f"{working_dir} is not a valid Git repository")
-    
+def get_url(full_name: str):
+    return f"https://github.com/{full_name}.git"
+
+
+def make_directory_for_bare_clones(
+    parent_dir: Path,
+    parents: bool = True,
+    exist_ok: bool = True
+):
+    bare_clones_dir = Path(parent_dir, "data/bare_clones")
+    bare_clones_dir.mkdir(parents=parents, exist_ok=exist_ok)
+    return bare_clones_dir
+
+
+def create_path_for_git_directory(
+    parent_dir: Path,
+    full_name_of_repo: str
+):
+    path = Path(parent_dir, f"{full_name_of_repo}.git")
+    raise_error_if_path_is_not_git_dir(path)
+    return path
+
+
+def raise_error_if_path_is_not_git_dir(path: Path):
+    if not path_is_git_dir(path):
+        raise ValueError(f"{path} is not a valid Git repository")
+
+
+def path_is_git_dir(path: Path):
+    return path.suffix == ".git"
+
+
+def run_git_gc(working_dir: Path):
+    raise_error_if_path_is_not_git_dir(working_dir)
     return subprocess.run(
         args=["git", "gc", "--aggressive", "--prune=now"],
         cwd=working_dir,
@@ -16,6 +48,28 @@ def run_git_gc(working_dir: str):
     )
 
 
-def path_is_git_dir(path: str):
-    path = Path(path)
-    return path.is_dir() and (path.name.endswith(".git") or (path / ".git").exists())
+def delete_git_dir(path: Path):
+    raise_error_if_path_is_not_git_dir(path)
+    repo_owner_dir = path.parent
+    rmtree(path)
+    if dir_is_empty(repo_owner_dir):
+        repo_owner_dir.rmdir()
+
+
+def dir_is_empty(path: Path):
+    return len(os.listdir(path)) == 0
+
+
+def main():
+    root = Path(__file__).resolve().parent.parent
+    bare_clones_dir = make_directory_for_bare_clones(root)
+    path = create_path_for_git_directory(parent_dir=bare_clones_dir, full_name_of_repo="leonardesser/autumn-2025")
+    path.mkdir(parents=True, exist_ok=True)
+    another_path = create_path_for_git_directory(parent_dir=bare_clones_dir, full_name_of_repo="leonardesser/winter-2025")
+    another_path.mkdir(parents=True, exist_ok=True)
+    delete_git_dir(path)
+    delete_git_dir(another_path)
+
+
+if __name__ == "__main__":
+    main()
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 
 from pathlib import Path
 
-from helpers import run_git_gc
+from helpers import get_url, make_directory_for_bare_clones, create_path_for_git_directory, run_git_gc, delete_git_dir
 from sampling import get_sample
 from calling_github import clone, get_commits
 import config
@@ -12,20 +12,22 @@ def main():
     sample = get_sample()
     for full_name in sample:
         commits = get_commits(
-            full_name=full_name,
-            path="README.md",
-            since=config.SINCE,
-            until=config.UNTIL
+            full_name,
+            config.FILE_TO_BE_STUDIED,
+            config.SINCE,
+            config.UNTIL
         )
-        url = f"https://github.com/{full_name}.git"
+        url = get_url(full_name)
         root = Path(__file__).resolve().parent.parent
-        path = os.path.join(root, f"data/clones/{full_name}.git")
-        if os.path.exists(path):
+        bare_clones_dir = make_directory_for_bare_clones(root)
+        path = create_path_for_git_directory(parent_dir=bare_clones_dir, full_name_of_repo=full_name)
+        if path.exists():
             print(f"Not cloning because the path already exists.")
         else:
-            repo = clone(url=url, path=path)
+            repo = clone(url=url, path=path, depth=1)
             result = run_git_gc(working_dir=path)
             print(f"Running git gc {'was successful' if result.returncode == 0 else 'failed'}.")
+        delete_git_dir(path)
 
 
 if __name__ == "__main__":
