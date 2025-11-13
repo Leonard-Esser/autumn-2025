e84f7a87a8cbdfa9df63110256618cb3c9ebe7f5
Leonard Esser <leonard.esser@posteo.net>
2025-11-13 08:49:42 +0100

    Add src directory and config.py
    - Move main.py to new src directory
    - Add config.py with some global constants
    - Import config in main.py and print SAMPLE_SIZE

---
+++ src/config.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+
+SAMPLE_SIZE = 1
+SINCE = datetime(2024, 1, 1)
+UNTIL = datetime(2024, 12, 31, 23, 59, 59)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
+import config
+
+
 def main():
     print("Hello from autumn-2025!")
+    print(f"The configured sample size is: {config.SAMPLE_SIZE}")
 
 
 if __name__ == "__main__":
