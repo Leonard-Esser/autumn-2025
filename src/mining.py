ff4dba33b28a4812b09502b3737c06c09b249301
Leonard Esser <leonard.esser@posteo.net>
2025-11-14 16:38:37 +0100

    Modify / Optimize mining.py
    - Get the right diff for each commit in early version of a classify
      function

--- src/mining.py
+++ src/mining.py
@@ -20,18 +20,18 @@ def get_file_specific_commits(repo, full_name_of_repo, commits, path):
 @timer
 def get_ccd_events(repo: Repository, full_name_of_repo, commits, path, classify_func):
     rows = [
-        classify_func(repo, full_name_of_repo, commit, path)
+        classify_func(repo, commit, path)
         for commit in commits
     ]
-    return pd.DataFrame(rows)
+    return pd.DataFrame(rows).assign(Repository=full_name_of_repo)
 
 
-def classify(repo: Repository, full_name_of_repo, commit: Commit, path):
-    if len(commit.parent_ids) == 0:
-        pass
-    diff = repo.diff(commit.id, commit.parent_ids[0])
+def classify(repo: Repository, commit: Commit, path):
+    if commit.parent_ids:
+        diff = repo.diff(commit.id, commit.parent_ids[0])
+    else:
+        diff = commit.tree.diff_to_tree()
     return {
-        "Repository": full_name_of_repo,
         "Commit": commit.id,
         "Time": commit.commit_time,
         "Path": path,
