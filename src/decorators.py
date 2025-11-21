b062b341f57589cc5116877c7fc27620a112b1db
Leonard Esser <leonard.esser@posteo.net>
2025-11-21 12:20:33 +0100

    Add "memory" to the programm
    - Add reminders collection for the authors of this project
    - Add decorators that remind authors of todos RE a function

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,8 @@
 import time
 from functools import wraps
 
+from memory import reminders
+
 
 def stop_the_clock(func):
     @wraps(func)
@@ -9,6 +11,28 @@ def stop_the_clock(func):
         result = func(*args, **kwargs)
         end = time.perf_counter()
         print(f"{func.__name__} took {end - start:.6f} seconds to execute.")
+        return result
+    return wrapper
+
+
+def explain_why(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        reminder = f"Do not forget to explain why {func.__name__} does what it does."
+        if reminder not in reminders:
+            reminders.append(reminder)
+        return result
+    return wrapper
+
+
+def back_up_with_literature(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        reminder = f"Back {func.__name__} up with literature."
+        if reminder not in reminders:
+            reminders.append(reminder)
         return result
     return wrapper
 
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 from batching import process_each_sample
-from decorators import stop_the_clock
+from decorators import stop_the_clock, explain_why, back_up_with_literature
 from helpers import get_version
 from io_helpers import export_sample, export_ccd_events
+from memory import reminders
 from sampling import get_sample
 import config
 import helpers
@@ -13,8 +14,15 @@ def get_root() -> Path:
     return Path(__file__).resolve().parent.parent
 
 
+@explain_why
+@back_up_with_literature
 def is_ccd_event(text: str) -> bool:
     return False
+
+
+def print_reminders():
+    for reminder in reminders:
+        print(reminder)
 
 
 @stop_the_clock
@@ -31,3 +39,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print("---")
+    print("Reminders:")
+    print_reminders()
