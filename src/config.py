93d22c9a5a35120bdfe75c950373ccd42824d184
Leonard Esser <leonard.esser@posteo.net>
2026-01-08 14:17:02 +0100

    Change config options

--- src/config.py
+++ src/config.py
@@ -22,7 +22,7 @@ _CONTRIBUTING = "CONTRIBUTING"
 # section: RE the subject
 RANDOM_STATE = 42
 NUMBER_OF_REPOS_TO_INVESTIGATE: int | None = None
-SINCE: datetime | None = None
+SINCE: datetime | None = datetime(2023, 1, 1, 0, 0, 0)
 # The datetime value of UNTIL has to be before the time of cloning a repo
 UNTIL: datetime | None = datetime(2025, 12, 31, 23, 59, 59)
 PATHS_TO_CONSIDER = [
@@ -35,21 +35,21 @@ PATHS_TO_CONSIDER = [
     _CONTRIBUTING + ".txt",
     _CONTRIBUTING.lower() + ".txt",
 ]
-ONLY_CLASSIFY_THIS_MANY_COMMITS_PER_REPO: int | None = None
+ONLY_CLASSIFY_THIS_MANY_COMMITS_PER_REPO: int | None = 10
 # end of section
 
 # section: program behavior
-RETURNS_CACHED_DATA_IF_ANY = False
+RETURNS_CACHED_DATA_IF_ANY = True
 UPDATES_CACHE = True
-CANNOT_DETECT_ANYTHING = True
-IS_NAYSAYER = True
+CANNOT_DETECT_ANYTHING = False
+IS_NAYSAYER = False
 DELETES_GIT_DIR_IMMEDIATELY = False
 # end of section
 
 # section: diffing options
 ASSUMES_MAXIMUM_OF_ONE_DELTA_PER_FILE = True
 CONTEXT_LINES = 0
-FINDS_SIMILAR = True
+FINDS_SIMILAR = False
 # end of section
 
 # section: RE the Zero-shot classifier via NLI
