e2db9c70f03bec631688e3557f5bc3e0f581e09e
Leonard Esser <leonard.esser@posteo.net>
2026-01-08 11:24:27 +0100

    Add empty communication channels keywords dict

---
+++ new_src/communication_channels.py
@@ -0,0 +1,62 @@
+KEYWORDS: dict[str, list[str]] = {
+    # one-way-channels
+    "website": [],
+    "blog": [],
+    "rss_feed": [],
+    "medium": [],
+    
+    # chat
+    "gitter": [],
+    "discord": [],
+    "slack": [],
+    "patreon": [],
+    "zulip": [],
+    "icq": [],
+    "telegram": [],
+    "irc": [],
+
+    # forum
+    "forum": [],
+    "stack_overflow": [],
+    "reddit": [],
+
+    # personal
+    "meetup": [],
+    "skype": [],
+
+    # mail-related
+    "newsletter": [],
+    "mailing_list": [],
+    "mail": [],
+    "form": [],
+    "google_group": [],
+
+    # social media
+    "youtube": [],
+    "facebook": [],
+    "twitter": [],
+    "linkedin": [],
+
+    # ticket system
+    "jira": [],
+    "issues": [],
+
+    # GitHub channels
+    "github_issues": [],
+    "pull_request": [
+        "github pull request",
+        "pull request",
+        "pull-request",
+        "pr",
+        "submit a pr",
+        "open a pr",
+    ],
+    "fork": [],
+    "ping_on_github": [],
+    "github_wiki": [],
+    "github_discussions": [
+        "github discussions",
+        "discussion",
+        "start a discussion",
+    ],
+}
