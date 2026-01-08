c27e876892de7c56ea4f0bd0869b352c79cfb2c5
Leonard Esser <leonard.esser@posteo.net>
2026-01-08 19:52:53 +0100

    Comfort feature: main plays sound when finished
    - only on macOS however

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import platform
+import subprocess
 from get_root import get_root
 from orchestration import pipeline
 from setup_logging import setup_logging
@@ -8,5 +10,21 @@ def main():
     pipeline()
 
 
+def play_sound() -> None:
+    if platform.system() != "Darwin": # if not on macOS
+        return
+
+    try:
+        subprocess.run(
+            ["afplay", "/System/Library/Sounds/Glass.aiff"],
+            check=False,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+    except Exception:
+        pass
+
+
 if __name__ == "__main__":
     main()
+    play_sound()
